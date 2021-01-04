c8cff0c20f67cd38165d308143e3f105439492a8
CubikZ <62304019+CubikZ@users.noreply.github.com>
2021-01-04 18:08:20 +0100

    Fonctions : Generation planning Par CM (avancement)

--- IHM/GestionPlanning/src/Canne/vue/Fonctions/Fonctions.java
+++ IHM/GestionPlanning/src/Canne/vue/Fonctions/Fonctions.java
@@ -5,8 +5,11 @@
  */
 package Canne.vue.Fonctions;
 
+import Canne.dao.Maria.MariaCategorieDao;
 import Canne.dao.Maria.MariaFilmDao;
+import Canne.dao.Maria.MariaSalleDao;
 import Canne.dao.Maria.MariaVipDao;
+import Canne.dao.Maria.MonMariaDbDataSource;
 import Canne.dao.modele.Film;
 import Canne.dao.modele.Vip;
 import java.sql.Connection;
@@ -20,17 +23,17 @@ import javax.sql.DataSource;
  */
 public class Fonctions {
 
-    static DataSource dataSourceDAO;
+    static DataSource dataSourceDAO = MonMariaDbDataSource.getMdbDataSource();
     static Connection connexionBD;
     static MariaFilmDao filmDao = new MariaFilmDao();;
     static List<Film> listFilms;
     
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws SQLException {
+        genererPlaning();
     }
     
     
-    public void genererPlaning() throws SQLException
+    public static void genererPlaning() throws SQLException
     {
         //connexion a la BD
         connexionBD = dataSourceDAO.getConnection();
@@ -43,19 +46,21 @@ public class Fonctions {
         
         //Long metrage
         
-        
+        //Seance = un film + une salle...
         //Un certain regard
-        
-        
-        
+        for(Film f : listFilms) 
+            if(f.getCategorie() == MariaCategorieDao.CM)
+                System.out.println(f);
+                //CM --> salle DEBussy ou Buñuel
+                //Debussy : 1,2,3,4,5,6,7,8
+                //Bunuel : 5,6,7,8,1,2,3,4
+			     
     }
     
     public void courtMetrage()
     {
        
-       /*for(Vip v : list) {
-				System.out.println(v);
-			}*/
+       
         
     }
     
