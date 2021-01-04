2b6466fc7b79a80bece23fa5291e8cb999d68196
CubikZ <62304019+CubikZ@users.noreply.github.com>
2021-01-04 17:02:09 +0100

    Ajout + commencement des fonctions pour génerer le planning automatiquement

---
+++ IHM/GestionPlanning/src/Canne/vue/Fonctions/Fonctions.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Canne.vue.Fonctions;
+
+import Canne.dao.Maria.MariaFilmDao;
+import Canne.dao.Maria.MariaVipDao;
+import Canne.dao.modele.Film;
+
+/**
+ *
+ * @author loann
+ */
+public class Fonctions {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+    
+    public void genererPlaning()
+    {
+        //Liste de tous les types de films
+        
+        //Long metrage
+        
+        //Un certain regard
+        
+        
+        
+    }
+    
+    public void courtMetrage()
+    {
+       MariaFilmDao filmDao;
+       filmDao = new MariaFilmDao();
+       
+        
+    }
+    
+    
+    
+}
