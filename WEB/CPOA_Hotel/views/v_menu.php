8974549956ef100c1bc19941d51fe89cb9b657c4
ROGi76 <77341879+ROGi76@users.noreply.github.com>
2021-01-14 09:20:09 +0100

    MAJ

--- WEB/CPOA_Hotel/views/v_menu.php
+++ WEB/CPOA_Hotel/views/v_menu.php
@@ -13,3 +13,16 @@
 
 
 
+<nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <ul class="nav navbar-nav">
+				<li <?php echo ($page=='accueil' ? 'class="active"':'')?>>
+					<a href="index.php">
+						<?= MENU_ACCUEIL ?>
+					</a>
+				</li>
+    </ul>
+  </div>
+</nav>
+
+
