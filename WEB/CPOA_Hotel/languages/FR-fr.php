8a7ff5959f5e3ea0c16d497f4614dc93cc25cc94
ROGi76 <77341879+ROGi76@users.noreply.github.com>
2021-01-18 16:52:34 +0100

    css

--- WEB/CPOA_Hotel/languages/FR-fr.php
+++ WEB/CPOA_Hotel/languages/FR-fr.php
@@ -3,6 +3,7 @@
 define('LOGO', 'Logo de la compagnie'); // Affiché si image non trouvée
 
 define('MENU_ACCUEIL','Accueil');
+define('DECONNECTER', 'Se deconnecter');
 
 define('TEXTE_PAGE_404','Oops, la page demandée n\'existe pas !');
 define('MESSAGE_ERREUR',"Une erreur s'est produite");
@@ -10,5 +11,4 @@ define('MESSAGE_ERREUR',"Une erreur s'est produite");
 define('TITRE', 'Hôtel');
 define('ERREUR_QUERY', 'Problème d\'accès à la base de données. Contactez l\'administrateur');
 
-define('TITRE_PAGE_ACCUEIL_TOUS', 'Toutes les photos');
-define('TITRE_PAGE_PHOTO_TOUS', 'Les détails sur cette photo');
+
