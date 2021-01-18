08f52ee9cd2a786f44d57ea929b49419624a947b
CubikZ <62304019+CubikZ@users.noreply.github.com>
2021-01-18 17:34:33 +0100

    Update configuration.php

--- WEB/CPOA_Hotel/config/configuration.php
+++ WEB/CPOA_Hotel/config/configuration.php
@@ -3,17 +3,17 @@
 const DEBUG = true; // production : false; dev : true
 
 // Accès base de données
-const BD_HOST = 'localhost';
-const BD_DBNAME = 'cpoa';
-const BD_USER = 'root';
-const BD_PWD = 'root';
+const BD_HOST = 'iutdoua-web.univ-lyon1.fr';
+const BD_DBNAME = 'p1911441';
+const BD_USER = 'p1911441';
+const BD_PWD = '456133';
 
 
 // Langue du site
 const LANG ='FR-fr';
 
 // Paramètres du site : nom de l'auteur ou des auteurs
-const AUTEUR = 'TANLET Quentin'; 
+const AUTEUR = 'TANLET Quentin / Mahdar-Recorbet Loann / Peuriere Aymeric'; 
 
 //dossiers racines du site
 define('PATH_CONTROLLERS','./controllers/c_');
